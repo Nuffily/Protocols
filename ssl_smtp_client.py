0a4297b594bc8a29bfbe201175496f685427a1a1
Nuffily <Nuffily@gmail.com>
2025-06-27 07:48:33 +0500

    Доработки

--- ssl_smtp_client.py
+++ ssl_smtp_client.py
@@ -10,94 +10,13 @@ import sys
 import uuid
 from ssl import SSLSocket
 
+
 class MySMTP:
-
-    def request(self, socket, request):
-        """Отправляет текст с request по сокету, затем читает из него и возвращает прочитанное"""
-        socket.send((request + '\r\n').encode('utf-8'))
-        recv_data = socket.recv(65535).decode('utf-8')
-        return recv_data
-
-    def get_auth_info(self, filename: str) -> (str, str):
-        """Возвращает username и password из файла"""
-        with open(filename) as file:
-            username = file.readline().strip()
-            password = file.readline().strip()
-            return username, password
 
     def __init__(self, auth_file: str):
         self.username, self.password = self.get_auth_info(auth_file)
         self.client_addr = ('smtp.yandex.ru', 465)
         self.DOT_LINE_PATTERN = re.compile(r'\s*\.+\s*')
-
-    def get_message(self, message_file: str) -> str:
-        """
-        Читает сообщение из файла и возвращает в виде строки.
-        Ко всем строчкам, состоящим только из точек добавляется точка
-        """
-        with open(message_file, encoding='utf-8') as file:
-            message_body = ''
-            lines = file.readlines()
-            for line in lines:
-                if self.DOT_LINE_PATTERN.fullmatch(line):
-                    message_body += "." + line
-                else:
-                    message_body += line
-
-        return message_body
-
-    def connect(self, client: socket, sender: str, rcpts: str) -> SSLSocket:
-        """Начинает сессию с почтовым сервером, и возвращает сокет, обернутый в SSL"""
-        client.connect(self.client_addr)
-        ssl_context = ssl.create_default_context()
-        ssl_client = ssl_context.wrap_socket(client, server_hostname=self.client_addr[0])
-        ssl_client.recv(1024)
-
-        self.request(ssl_client, f'ehlo {self.username}@ya.ru')
-
-        base64login = base64.b64encode(self.username.encode()).decode()
-        base64password = base64.b64encode(self.password.encode()).decode()
-
-        self.request(ssl_client, 'AUTH LOGIN')
-
-        self.request(ssl_client, base64login)
-        self.request(ssl_client, base64password)
-        self.request(ssl_client, f'MAIL FROM:{sender}')
-
-        for rcpt in rcpts:
-            self.request(ssl_client, f"RCPT TO:{rcpt}")
-
-        self.request(ssl_client, 'DATA')
-
-        return ssl_client
-
-    def add_attachment(self, attachment_path: str, boundary: str, ) -> str:
-        """Возвращает текст с приложением по заданному пути, готовый для добавления с телу SMTP"""
-        if not os.path.exists(attachment_path):
-            print(f"Warning: Attachment file not found: {attachment_path}")
-            return ""
-
-        filename = os.path.basename(attachment_path)
-        mimetype, _ = mimetypes.guess_type(attachment_path)
-
-        if mimetype is None:
-            mimetype = 'application/octet-stream'
-
-        message = f'--{boundary}\r\n'
-        message += f'Content-Type: {mimetype}; name="{filename}"\r\n'
-        message += 'Content-Transfer-Encoding: base64\r\n'
-
-        encoded_filename = f'=?UTF-8?B?{base64.b64encode(filename.encode("utf-8")).decode("utf-8")}?='
-        message += f'Content-Disposition: attachment; filename="{encoded_filename}"\r\n\r\n'
-
-        with open(attachment_path, 'rb') as f:
-            attachment_data = f.read()
-            encoded_attachment = base64.b64encode(attachment_data).decode('utf-8')
-
-            for i in range(0, len(encoded_attachment), 76):
-                message += encoded_attachment[i:i + 76] + '\r\n'
-
-        return message
 
     def send(self, configure_file: str, message_file: str):
         """
@@ -130,7 +49,6 @@ class MySMTP:
                 elif key == "To":
                     message_headers += f'{key}: {", ".join(val)}\r\n'
 
-            print(message_headers)
             message = message_headers + "MIME-Version: 1.0\r\n"
             message_body = self.get_message(message_file)
 
@@ -157,9 +75,88 @@ class MySMTP:
             print(self.request(ssl_client, "."))
             print(self.request(ssl_client, "QUIT"))
 
+    def request(self, socket, request):
+        """Отправляет текст с request по сокету, затем читает из него и возвращает прочитанное"""
+        socket.send((request + '\r\n').encode('utf-8'))
+        recv_data = socket.recv(65535).decode('utf-8')
+        return recv_data
+
+    def get_auth_info(self, filename: str) -> (str, str):
+        """Возвращает username и password из файла"""
+        with open(filename) as file:
+            username = file.readline().strip()
+            password = file.readline().strip()
+            return username, password
+
+    def connect(self, client: socket, sender: str, rcpts: str) -> SSLSocket:
+        """Начинает сессию с почтовым сервером, и возвращает сокет, обернутый в SSL"""
+        client.connect(self.client_addr)
+        ssl_context = ssl.create_default_context()
+        ssl_client = ssl_context.wrap_socket(client, server_hostname=self.client_addr[0])
+        ssl_client.recv(1024)
+
+        self.request(ssl_client, f'ehlo {self.username}@ya.ru')
+        self.request(ssl_client, 'AUTH LOGIN')
+
+        base64login = base64.b64encode(self.username.encode()).decode()
+        base64password = base64.b64encode(self.password.encode()).decode()
+
+        self.request(ssl_client, base64login)
+        self.request(ssl_client, base64password)
+        self.request(ssl_client, f'MAIL FROM:{sender}')
+
+        for rcpt in rcpts:
+            self.request(ssl_client, f"RCPT TO:{rcpt}")
+
+        self.request(ssl_client, 'DATA')
+
+        return ssl_client
+
+    def get_message(self, message_file: str) -> str:
+        """
+        Читает сообщение из файла и возвращает в виде строки.
+        Ко всем строчкам, состоящим только из точек добавляется точка
+        """
+        with open(message_file, encoding='utf-8') as file:
+            message_body = ''
+            lines = file.readlines()
+            for line in lines:
+                if self.DOT_LINE_PATTERN.fullmatch(line):
+                    message_body += "." + line
+                else:
+                    message_body += line
+
+        return message_body
+
+    def add_attachment(self, attachment_path: str, boundary: str, ) -> str:
+        """Возвращает текст с приложением по заданному пути, готовый для добавления с телу SMTP"""
+        if not os.path.exists(attachment_path):
+            print(f"Warning: Attachment file not found: {attachment_path}")
+            return ""
+
+        filename = f'=?UTF-8?B?{base64.b64encode(os.path.basename(attachment_path).encode("utf-8")).decode("utf-8")}?='            # Имя без пути
+        mimetype, _ = mimetypes.guess_type(attachment_path)
+
+        if mimetype is None:
+            mimetype = 'application/octet-stream'
+
+        message = f'--{boundary}\r\n'
+        message += f'Content-Type: {mimetype}; name="{filename}"\r\n'
+        message += 'Content-Transfer-Encoding: base64\r\n'
+
+        message += f'Content-Disposition: attachment; filename="{filename}"\r\n\r\n'
+
+        with open(attachment_path, 'rb') as f:
+            attachment_data = f.read()
+            encoded_attachment = base64.b64encode(attachment_data).decode('utf-8')
+
+            for i in range(0, len(encoded_attachment), 76):
+                message += encoded_attachment[i:i + 76] + '\r\n'
+
+        return message
+
 
 def main():
-
     parser = argparse.ArgumentParser(
         description='Отправка email через SMTP',
         usage="""
@@ -185,6 +182,7 @@ def main():
 
     server = MySMTP(login_file)
     server.send(headers_file, message_file)
+
 
 if __name__ == "__main__":
     main()
